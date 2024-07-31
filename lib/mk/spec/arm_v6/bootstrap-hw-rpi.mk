26e787fc3b51629a21799fdeccbe0afc12698797
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2024-07-31 16:25:21 +0200

    hw: introduction of cpu local mem area
    Ref genodelabs/genode#5310

--- lib/mk/spec/arm_v6/bootstrap-hw-rpi.mk
+++ lib/mk/spec/arm_v6/bootstrap-hw-rpi.mk
@@ -2,13 +2,15 @@ REQUIRES = hw
 
 REP_INC_DIR += src/bootstrap/board/rpi
 
+SRC_CC  += bootstrap/platform_cpu_memory_area.cc
 SRC_CC  += board/rpi/platform.cc
 SRC_CC  += bootstrap/spec/arm/arm_v6_cpu.cc
-SRC_CC  += hw/spec/32bit/memory_map.cc
 SRC_S   += bootstrap/spec/arm/crt0.s
 
 vpath board/rpi/platform.cc $(REP_DIR)/src/bootstrap
 
 CC_MARCH = -march=armv6k+nofp -mfpu=vfp
+
+ARCH_WIDTH_PATH := spec/32bit
 
 include $(call select_from_repositories,lib/mk/bootstrap-hw.inc)
